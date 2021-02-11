2011fdd574119fdf91367e96de952582ac0bbe05
Heber <heberlazo@gmail.com>
2021-02-11 11:09:11 -0500

    Add models Authors, Posts and Comments

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  validates :body, presence: true
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { in: 4..14 }
+    validates :email, presence: true
+    validates :password, presence: true, length: { in: 6..16}
+    has_many :posts
+    has_many :comments
+  end
+  

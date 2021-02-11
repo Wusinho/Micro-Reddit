dd3494c04e0a7f83b42f1e4fd5831efc0492f3da
Heber <heberlazo@gmail.com>
2021-02-11 14:46:43 -0500

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
   validates :body, presence: true
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 4..14 }
   validates :email, presence: true
